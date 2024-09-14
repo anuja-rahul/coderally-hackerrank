a3a444b2f8673f95cc2a265e5851788d5dc1c398
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-09-14 12:00:45 +0530

    added 1

---
+++ 1.biological_hazards.py
@@ -0,0 +1,48 @@
+from typing import List
+
+
+def bioHazards(n: int, allergic: List[int], poisonous: List[int]) -> int:
+    all_bacteria = [b for b in range(1, n+1)]
+    universal_sample = all_possible_outcomes(all_bacteria)
+    exclude = []
+    for i in range(len(allergic)):
+        current_set = [allergic[i], poisonous[i]]
+        exclude.append(current_set)
+    # print(universal_sample)
+    # print(len(universal_sample))
+
+    test_list = []
+
+    for items in universal_sample:
+        if items in exclude:
+            test_list.append(items)
+
+    for data in universal_sample:
+        for item in exclude:
+            # print(f"data: {data}, item: {item}, result: {item[0] not in data and item[1] not in data}")
+            if item[0] in data and item[1] in data:
+                test_list.append(data)
+
+    temp = []
+    for item in universal_sample:
+        if item not in test_list:
+            temp.append(item)
+
+    return len(universal_sample)
+
+
+def all_possible_outcomes(data_list: List[int]) -> List[List[int]]:
+    subsets = []
+    for i in range(len(data_list)):
+        for j in range(i + 1, len(data_list) + 1):
+            subset = data_list[i:j]
+            subsets.append(subset)
+    return subsets
+
+
+def filter_list(data_list, matching_str):
+    return [item for item in data_list if matching_str not in item]
+
+
+bioHazards(4, [1, 2], [3, 4])
+# 7
